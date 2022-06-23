5443711d19f4bda7ec14cbc62d048163edbf7b7a
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2022-06-23 19:57:37 -0300

    chore: clear default nest files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { EnvironmentConfigModule } from './infra/config/environment-config/environment-config.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EnvironmentConfigModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
