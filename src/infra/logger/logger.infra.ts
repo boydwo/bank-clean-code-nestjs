3488b39da19ac4b5a9ad4670914fa56514758e3e
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2022-06-26 17:16:35 -0300

    feat: create logger infra config

---
+++ src/infra/logger/logger.infra.ts
@@ -0,0 +1,24 @@
+import { Logger } from '@nestjs/common';
+import { ILogger } from 'src/domain/protocols/logger/logger.interface';
+
+export class LoggerService extends Logger implements ILogger {
+  debug(context: string, message: string) {
+    if (process.env.NODE_ENV !== 'production') {
+      super.debug(`[DEBUG] ${message}`, context);
+    }
+  }
+  info(context: string, message: string) {
+    super.log(`[INFO] ${message}`, context);
+  }
+  error(context: string, message: string, trace?: string) {
+    super.error(`[ERROR] ${message}`, trace, context);
+  }
+  warn(context: string, message: string) {
+    super.warn(`[WARN] ${message}`, context);
+  }
+  verbose(context: string, message: string) {
+    if (process.env.NODE_ENV !== 'production') {
+      super.verbose(`[VERBOSE] ${message}`, context);
+    }
+  }
+}
