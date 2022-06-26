b5387836c85a5137d1a7a8812f6111ed14a0d020
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2022-06-26 11:17:37 -0300

    refactor: database client interface

--- src/domain/protocols/database/databaseClient.interface.ts
+++ src/domain/protocols/database/databaseClient.interface.ts
@@ -1,8 +1,7 @@
 export interface IDatabaseClient {
   create(data: any): Promise<any>;
   findAll(): Promise<any[]>;
-  findBy(data: number): Promise<any>;
-  update(data: any): Promise<void>;
+  findBy(data: any): Promise<any>;
+  update(data: any): Promise<any>;
   delete(data: any): Promise<void>;
-  save(data: any): Promise<void>;
 }
