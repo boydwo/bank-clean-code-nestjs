508c2346aaae6d8eb948d7d6d6e8b2bf6da1f6e8
Marcos Tulio Pajé <marcostulio06@hotmail.com>
2022-06-30 12:37:05 -0300

    chore: add configs swagger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Ripio Trade')
+    .setDescription(
+      'This is the OpenAPI specifications for the Ripio Trade API'
+    )
+    .setVersion('1.0')
+    .addTag('Banking System')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
